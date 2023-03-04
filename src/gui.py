2f891d8334845da6433f4cbfecd4b7d3cbe9d2f4
Łukasz Kuk <lukasz0104201@wp.pl>
2023-03-04 23:26:03 +0100

    refactor

--- src/gui.py
+++ src/gui.py
@@ -2,39 +2,53 @@ import PySimpleGUI as Sg
 from src import functions
 
 
+class TodoWindow:
+    def __init__(self):
+        self.window = Sg.Window("Todo app",
+                                layout=self.__class__._layout(),
+                                font=("Helvetiva", 20))
+
+    @staticmethod
+    def _layout():
+        label = Sg.Text("Type in a todo")
+        input_box = Sg.InputText(tooltip="Enter todo", key="todo")
+        add_button = Sg.Button("Add")
+        list_box = Sg.Listbox(values=functions.get_todos(), key="todos",
+                              enable_events=True, size=(45, 10))
+        edit_button = Sg.Button("Edit")
+        return [[label], [input_box, add_button], [list_box, edit_button]]
+
+    def process_user_input(self):
+        while True:
+            event, values = self.window.read()
+            print(event)
+            print(values)
+            match event:
+                case "Add":
+                    self._add_todo(values["todo"])
+                case "Edit":
+                    self._edit_todo(values["todos"][0], values["todo"])
+                case Sg.WIN_CLOSED:
+                    break
+
+        self.window.close()
+
+    @staticmethod
+    def _add_todo(new_todo):
+        if not new_todo:
+            return
+        todos = functions.get_todos()
+        todos.append(new_todo + "\n")
+        functions.write_todos(todos)
+
+    def _edit_todo(self, todo_to_edit, new_todo):
+        todos = functions.get_todos()
+        index = todos.index(todo_to_edit)
+        todos[index] = new_todo + "\n"
+        functions.write_todos(todos)
+        self.window["todos"].update(values=todos)
+
+
 if __name__ == "__main__":
-    label = Sg.Text("Type in a todo")
-    input_box = Sg.InputText(tooltip="Enter todo", key="todo")
-    add_button = Sg.Button("Add")
-    list_box = Sg.Listbox(values=functions.get_todos(), key="todos",
-                          enable_events=True, size=(45, 10))
-    edit_button = Sg.Button("Edit")
-
-    window = Sg.Window("Todo app",
-                       layout=[[label], [input_box, add_button], [list_box, edit_button]],
-                       font=("Helvetiva", 20))
-
-    while True:
-        event, values = window.read()
-        print(event)
-        print(values)
-        match event:
-            case "Add":
-                if not values["todo"]:
-                    continue
-                todos = functions.get_todos()
-                new_todos = values["todo"] + "\n"
-                todos.append(new_todos)
-                functions.write_todos(todos)
-            case "Edit":
-                todo_to_edit = values["todos"][0]
-                new_todo = values["todo"] + "\n"
-                todos = functions.get_todos()
-                index = todos.index(todo_to_edit)
-                todos[index] = new_todo
-                functions.write_todos(todos)
-                window["todos"].update(values=todos)
-            case Sg.WIN_CLOSED:
-                break
-
-    window.close()
+    handler = TodoWindow()
+    handler.process_user_input()
