30cdeaadaf0f794d59e5272ca5c1285e524fcaf5
Łukasz Kuk <lukasz0104201@wp.pl>
2023-03-07 00:49:04 +0100

    Heroku deployment config

---
+++ setup.sh
@@ -0,0 +1,13 @@
+mkdir -r ~/.streamlit/
+
+echo "\
+[general]\n\
+email = \"email@domain\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
